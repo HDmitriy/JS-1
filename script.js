9de9b95866ddaf7ab5ace130cea400af302ceb0f
HDmitriy <76693042+HDmitriy@users.noreply.github.com>
2021-01-11 17:19:09 +0300

    hw 3 complete

--- script.js
+++ script.js
@@ -1,117 +1,53 @@
-// №1
-let a = 1, b = 1, c, d;
-c = ++a; alert(c);           // 2  ++ перед переменной добавляет единицу и возвращает новое значение
-d = b++; alert(d);           // 1  ++ после переменной добавляет единицу и возвращает старое значение (до увеличения числа).
-c = (2+ ++a); alert(c);      // 5  2+3=5
-d = (2+ b++); alert(d);      // 4  2+2=4
-alert(a);                    // 3  1+1+1=3 
-alert(b);                    // 3  1+1+1=3 
+// №1 
+let numb = 0;
+   while (numb <= 100) {
+       let check = true;
+       for (let i = 2; i < numb; i++){
+           if (numb%i === 0) {
+               check = false;
+               break;
+           }
+       }
+       if (check) console.log(numb);
+       numb++;
+   } 
 
+// №2 
 
-// №2
-let f = 2;
-let x = 1 + (f *= 2); // x = 5, т.к *= умножение, в котором получившееся число сразу же присваивается
-alert(x);
+let basket = [
+    {
+    	product: 'Тетрадь',
+    	price: 100
+    },
+    {
+    	product: 'Ручка',
+    	price: 25
+    },
+    {
+   		product: 'Карандаш',
+    	price: 10
+    },
+    {
+    	product: 'Линейка',
+    	price: 50
+    }
+];
+let basketPrice = 0;
+    for (let prod of basket){
+        basketPrice += prod.price;
+        console.log(prod.product + ' стоит: ' + prod.price + ' руб.');
+    }
+
+// console.log('Все в товары корзине стоят: ' + basketPrice + ' руб.'); - одно и тоже с №3
 
 // №3
-let g = 8;
-let h = 2;
-if (g >= 0 && h >= 0){
-	z = g - h;
-	alert(z);
-} else if (g < 0 && h < 0){
-	z = g * h;
-	alert (z);
 
-} else if (g > 0 && h < 0 || g < 0 && h > 0){
-	z = g + h;
-	alert (z);
+function countBasketPrice(basket) {
+    let funBasketPrice = 0;
+    for (let prod of basket){
+        funBasketPrice += prod.price;
+    }
+    return funBasketPrice;
 }
 
-// №4
-let v = +prompt ('Введите число от 1 до 15');
-	switch (v) {
-	case 1:
-		alert ( 'Ваше число 1');
-		break;
-	case 2:
-		alert ( 'Ваше число 2');
-		break;
-	case 3:
-		alert ( 'Ваше число 3');
-		break;
-	case 4:
-		alert ( 'Ваше число 4');
-		break;
-	case 5:
-		alert ( 'Ваше число 5');
-		break;
-	case 6:
-		alert ( 'Ваше число 6');
-		break;
-	case 7:
-		alert ( 'Ваше число 7');
-		break;
-	case 8:
-		alert ( 'Ваше число 8');
-		break;
-	case 9:
-		alert ( 'Ваше число 9');
-		break;
-	case 10:
-		alert ( 'Ваше число 10');
-		break;
-	case 11:
-		alert ( 'Ваше число 11');
-		break;
-	case 12:
-		alert ( 'Ваше число 12');
-		break;
-	case 13:
-		alert ( 'Ваше число 13');
-		break;
-	case 14:
-		alert ( 'Ваше число 14');
-		break;
-	case 15:
-		alert ( 'Ваше число 15');
-		break;	
-	}	
-
-// №5
-let k = 6;
-let j = 4;
-		
-	function plus(k, j) {
-    	return k + j;
-	}
-
-	function minus(k, j) {
-    	return k - j;
-	}
-
-	function div (k, j) {
-    	return k / j;
-	}
-
-	function mult (k, j) {
-    	return k * j;
-	}
-
-// №6
-function mathOperation(arg1, arg2, operation){
-    switch(operation){
-       	case 'сложение':
-        	return arg1 + arg2;
-           	break;
-       	case 'вычитание':
-           	return arg1 - arg2;
-            break;
-        case 'деление':
-            return arg1 / arg2;
-            break;
-        case 'умножение':
-           	 return arg1 * arg2;
-             break;
-   	}
-}
+console.log('Все в товары корзине стоят: ' + countBasketPrice(basket) +  ' руб.');
